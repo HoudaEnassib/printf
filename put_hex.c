82120073707a6157c77f713535ed7f31776c0920
HoudaEnassib <hnour6525@gmail.com>
2023-10-17 14:19:18 +0000

    put_hex

--- put_hex.c
+++ put_hex.c
@@ -23,3 +23,26 @@ int	_putaddress(void *addr)
 	free(str);
 	return (i);
 }
+
+/**
+ * _put_hex - Custom function to write a
+ * hexadecimal number to the standard output.
+ *
+ * @num: The hexadecimal number to be written.
+ * @flag: Flag indicating if the number should
+ * include the "0x" prefix (1 for yes, 0 for no).
+ *
+ * Return: The number of characters written.
+ */
+
+int	_put_hex(unsigned int num, int flag)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = ft_itoa_hex(num, flag);
+	i += _putstr(str);
+	free(str);
+	return (i);
+}
